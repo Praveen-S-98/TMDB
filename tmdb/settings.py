c5f7d12217fb1581f616c6bda1df85b47de4d67b
praveen <praveenmadrass98@gmail.com>
2020-09-01 16:41:09 +0530

    modified

--- tmdb/settings.py
+++ tmdb/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
